95e7947fa3e509c2944e44f44804e5f867508fa2
Divya gupta <53807822+Divya19gupta@users.noreply.github.com>
2020-11-23 17:37:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  const [ people , setPerson ] = useState(data);
-  return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people}/>
-        <button onClick={()=>setPerson([])}>Clear All</button>
-      </section>
-    </main>
-  );
+import './index.css';
+
+class App extends Component{
+
+    state={
+      people:data
+    }
+    setToZero =()=>{
+      let p = [];
+      this.setState({people:[]})
+    }
+    render(){
+      return(
+        <main>
+        <div className="container">
+          <h3> {data.length} Birthdays today</h3>
+          <List people={this.state.people}/>
+          <button onClick={this.setToZero}>Clear</button>
+        </div>
+        </main>
+      )
+    }
 }
+
+
+// function App() {
+//   const [ people , setPerson ] = useState(data);
+//   return (
+//     <main>
+//       <section className="container">
+//         <h3>{people.length} birthdays today</h3>
+//         <List people={people}/>
+//         <button onClick={()=>setPerson([])}>Clear All</button>
+//       </section>
+//     </main>
+//   );
+// }
 
 export default App;
